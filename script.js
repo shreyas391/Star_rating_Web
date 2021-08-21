d32499c59ac2ed04bcadbc8c12b5e3bc258f8c3d
Shreyas2007 <84057370+shreyas391@users.noreply.github.com>
2021-08-21 21:34:27 +0530

    complete full main js

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
-const f_1 = document.querySelector("#f_1")
-const f_2 = document.querySelector("#f_2")
-
-function firstStar() {
-    f_1.style.backgroundColor = "yellow"
-    for (let i = 2; i <= 5; i++) {
-        document.getElementById('f_' + i).style.backgroundColor = 'aqua'
+var num = "ntg";
+const yesFun = () => {
+    if (num == "no") {
+        console.log("error");
     }
+    else{
+        $(".yes").css("backgroundColor", "#9DDAC6")
+        num = "yes"
+    } 
 }
 
-function secondStar() {
-    f_1.style.backgroundColor = "yellow"
-    f_2.style.backgroundColor = "yellow"
-    for (let i = 3; i <= 5; i++) {
-        document.getElementById('f_' + i).style.backgroundColor = 'aqua'
+const noFun = () => {
+    if (num == "yes") {
+        console.log("error");
     }
+    else{
+        $(".no").css("backgroundColor", "#9DDAC6")
+        num = "no"
+
+    } 
 }
 
-function thirdStar() {
-    for (let i = 1; i <= 3; i++) {
-        document.getElementById('f_' + i).style.backgroundColor = 'yellow'
-    }
-    for (let i = 4; i <= 5; i++) {
-        document.getElementById('f_' + i).style.backgroundColor = 'aqua'
-    }
-}
+const subFun = () => {
+    if (num == "yes" || num == "no") {
+    const btn = document.getElementById("btn")
+    btn.style.transform = "rotate(180deg)"
+    btn.style.backgroundColor = "green"
+    btn.style.width = "80px"
+    btn.style.borderRadius = "50%"
+    btn.style.height = "80px"
+    btn.style.position = "relative"
+    btn.style.left = "41%"
+    btn.innerHTML = "<svg width=\"58\" height=\"45\" viewBox=\"0 0 58 45\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#fff\" fill-rule=\"nonzero\" d=\"M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65\"/></svg>"
 
-function fourthStar() {
-    for (let i = 1; i <= 4; i++) {
-        document.getElementById('f_' + i).style.backgroundColor = 'yellow'
+    var all = 1;
+    const func = () => {
+        all++
+        if (all == 2) {
+           clearInterval(interval)
+            if (num == "yes") {
+                document.location.href="Pages/yesPage.html"
+            }
+            else if (num == "no") {
+                document.location.href="Pages/noPage.html"
+            }
+        }
     }
-    for (let i = 5; i <= 5; i++) {
-       document.getElementById('f_' + i).style.backgroundColor = 'aqua'
+    const interval = setInterval(func , 1000);        
     }
-}
-
-function fifthStar() {
-    for (let i = 1; i <= 5; i++) {
-        document.getElementById('f_' + i).style.backgroundColor = 'yellow'
+    else {
+        console.log("error");
     }
 }
